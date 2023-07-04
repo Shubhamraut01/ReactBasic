113f22ee949c2b5ee482fb71c207ff6a5367df91
Shubham Raut <shubhamrao12321@gmail.com>
2023-07-04 16:35:36 +0530

    Update main.jsx

--- src/Context Hook/main.jsx
+++ src/Context Hook/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import File2 from "./File2";
 
-const NameContext = createContext();
+const NameContext = createContext(); //creating context
 const myname = "shubham";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <React.StrictMode>
-    <NameContext.Provider value={myname}>
+    
+    <NameContext.Provider value={myname}>  //wraping context_name.Provider and assigning value to it
       <App />
     </NameContext.Provider>
+    
   </React.StrictMode>
 );
 
-export { NameContext };
+export { NameContext }; //exporting context
